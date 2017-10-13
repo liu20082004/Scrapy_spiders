bcc4ce51a8564bac277c97039f37cde4b4c9fc59
liu2008.2004 <liu2008.2004@163.com>
2017-10-13 14:12:20 +0800

    1.初始化时清空dytt8文件 2.优化提取下载链接的正则表达式

--- a1/spiders/www_dytt8_net.py
+++ a1/spiders/www_dytt8_net.py
@@ -13,6 +13,11 @@ class dytt8Spider(Spider):
 	# header = { 'Accept':'image/webp,image/*,*/*;q=0.8', 'Accept-Encoding':'gzip, deflate, sdch','Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6','Connection':'keep-alive' }
 	# USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
 
+	def __init__(self):
+		"""初始化函数,暂时用来创建文档"""
+		fp = open("dytt8.txt", "w")
+		fp.close()
+
 	def parse(self, response):
 		filmTypes = response.xpath('//div[@class="bd3r"]//div[@class="co_area2"]')  # 返回的是selectorList的对象
 		for filmType in filmTypes:
@@ -35,12 +40,12 @@ class dytt8Spider(Spider):
 
 
 	def parse_download(self, response):
-		pattern = re.compile('href="ftp://.*[\.mkv|\.rmvb|\.mp4|\.exe|\.rar]"', re.I)
+		pattern = re.compile('="\s*(?:ftp|http)://.*\.(?:mkv|rmvb|mp4|exe|rar)\s*"', re.I)  #正则表达式  分组()  (?:)非分组捕获,用于使用|
 		ftpURLs = re.findall( pattern, response.text)
 		downloadURL = []
 		for ftpURL in ftpURLs:
 			print ftpURL
-			downloadURL.append(ftpURL[6:-1].encode('utf8'))
+			downloadURL.append(ftpURL[2:-1].encode('utf8'))
 
 		film_item = [ a for a in self.film_items if a['href'] in response._url ]
 		# film_item = self.film_items[ self.filmID ]
